88f2132bf39ce704299e0f41f31606d5d6388d2a
lump-of-sugar <12610204@sankogakuen.jp>
2026-07-24 00:45:55 +0900

    Update page2.js

--- page2.js
+++ page2.js
@@ -276,7 +276,7 @@ function success(){
 
     redOverlay.classList.remove("warningFlash");
 
-    timerDisplay.textContent = "\(*ˊᗜˋ*)/";
+    timerDisplay.textContent = "(,,>᎑<,,)";
 
     showMessage("爆弾の無力化に成功しました");
 
